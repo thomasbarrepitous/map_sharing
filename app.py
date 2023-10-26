87488b25aee9025e89763a381dcdc2a2c27ded59
Thomas Barré-Pitous <barrepitousthomas@gmail.com>
2023-10-26 22:25:28 +0900

    Multi page implementation + login button

--- app.py
+++ app.py
@@ -1,109 +1,22 @@
+import dash
 from dash import Dash, Output, Input, State, html
-import dash_mantine_components as dmc
-import googlemaps
-import os
-import uuid
-import dash_leaflet as dl
 
 app = Dash(
     __name__,
+    suppress_callback_exceptions=True,
     external_stylesheets=[
         # include google fonts
-        "https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;900&display=swap"
+        "https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;900&display=swap",
     ],
+    use_pages=True,
 )
 
-# API Auth
-gmaps = googlemaps.Client(key=os.environ.get("GCP_KEY"))
-session_token = uuid.uuid4().hex
-
-container_style = {
-    "marginTop": 20,
-    "marginBottom": 20,
-}
 
 app.layout = html.Div(
     [
-        dmc.Container(
-            [
-                dmc.Header(
-                    height=60,
-                    children=[dmc.Text("Map Sharing")],
-                    style={"backgroundColor": "#9c86e2"},
-                )
-            ],
-            style=container_style,
-        ),
-        dmc.Container(
-            [
-                dmc.Select(
-                    data=[],
-                    value="",
-                    clearable=True,
-                    nothingFound="No results found",
-                    searchable=True,
-                    id="select",
-                ),
-            ],
-            style=container_style,
-        ),
-        dmc.Container(
-            [
-                html.Div(
-                    dl.Map(
-                        [dl.TileLayer()],
-                        center=[40, 15],
-                        zoom=2,
-                        id="map",
-                        style={"height": "50vh", "z-index": 0, "tabindex": -2},
-                    ),
-                    id="map-container",
-                ),
-            ],
-            style=container_style,
-        ),
+        dash.page_container,
     ]
 )
-
-
-@app.callback(
-    Output("select", "data"),
-    [Input("select", "searchValue")],
-    prevent_initial_call=True,
-)
-def update_select(search_query):
-    if search_query:
-        try:
-            # Fetch data from the API
-            search_results = gmaps.places_autocomplete(
-                session_token=session_token, input_text=search_query
-            )
-            # Create a map using the fetched data
-            return [
-                {"value": result["place_id"], "label": result["description"]}
-                for result in search_results
-            ]
-        except Exception as e:
-            print("ERROR : ", e)
-    return []
-
-
-@app.callback(
-    Output("map", "viewport"),
-    [Input("select", "value")],
-    prevent_initial_call=True,
-)
-def update_map(select_value):
-    if select_value:
-        try:
-            geocode_result = gmaps.geocode(place_id=select_value)
-            geocode_lon = geocode_result[0]["geometry"]["location"]["lng"]
-            geocode_lat = geocode_result[0]["geometry"]["location"]["lat"]
-            # Create a map using the fetched data
-            return dict(center=[geocode_lat, geocode_lon], zoom=11, transition="flyTo")
-        except Exception as e:
-            print("ERROR : ", e)
-    return {}
 
 
 if __name__ == "__main__":
---
+++ pages/home.py
@@ -0,0 +1,121 @@
+import dash
+from dash import callback, Output, Input, State, html
+import dash_mantine_components as dmc
+import googlemaps
+import os
+import uuid
+import dash_leaflet as dl
+from dash_iconify import DashIconify
+
+dash.register_page(__name__, path="/")
+
+
+# API Auth
+gmaps = googlemaps.Client(key=os.environ.get("GCP_KEY"))
+session_token = uuid.uuid4().hex
+
+
+layout = html.Div(
+    [
+        dmc.Container(
+            [
+                dmc.Header(
+                    height=60,
+                    children=[
+                        dmc.Text("Map Sharing"),
+                        dmc.Anchor(
+                            dmc.Button(
+                                "Login",
+                                leftIcon=DashIconify(
+                                    icon="carbon:settings-check", width=20
+                                ),
+                            ),
+                            href="/login",
+                        ),
+                    ],
+                    style={"backgroundColor": "red"},
+                )
+            ],
+            style={"marginTop": 20, "marginbottom": 20},
+        ),
+        dmc.Container(
+            [
+                dmc.Select(
+                    data=[],
+                    value="",
+                    clearable=True,
+                    nothingFound="No results found",
+                    searchable=True,
+                    id="select",
+                ),
+            ],
+            style={"marginTop": 20, "marginbottom": 20},
+        ),
+        dmc.Container(
+            [
+                html.Div(
+                    dl.Map(
+                        [
+                            dl.TileLayer(),
+                        ],
+                        center=[40, 15],
+                        zoom=2,
+                        id="map",
+                        style={"height": "50vh", "z-index": 0, "tabindex": -2},
+                    ),
+                    id="map-container",
+                ),
+            ],
+            style={"marginTop": 20, "marginbottom": 20},
+        ),
+    ]
+)
+
+
+@callback(
+    Output("select", "data"),
+    [Input("select", "searchValue")],
+    prevent_initial_call=True,
+)
+def update_select(search_query):
+    if search_query:
+        try:
+            # Fetch data from the API
+            search_results = gmaps.places_autocomplete(
+                session_token=session_token, input_text=search_query
+            )
+            # Create a map using the fetched data
+            return [
+                {"value": result["place_id"], "label": result["description"]}
+                for result in search_results
+            ]
+        except Exception as e:
+            print("ERROR : ", e)
+    return []
+
+
+@callback(
+    Output("map", "viewport"),
+    Output("map", "children"),
+    [Input("select", "value")],
+    prevent_initial_call=True,
+)
+def update_map(select_value):
+    if select_value:
+        try:
+            geocode_result = gmaps.geocode(place_id=select_value)
+            geocode_lon = geocode_result[0]["geometry"]["location"]["lng"]
+            geocode_lat = geocode_result[0]["geometry"]["location"]["lat"]
+            map_viewport = dict(
+                center=[geocode_lat, geocode_lon], zoom=11, transition="flyTo"
+            )
+            map_children = [
+                dl.TileLayer(),
+                dl.Marker(position=[geocode_lat, geocode_lon]),
+            ]
+            return map_viewport, map_children
+        except Exception as e:
+            print("ERROR : ", e)
+    return {}, [
+        dl.TileLayer(),
+    ]
