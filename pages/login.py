724ecfcf113215f1b83d56b0a8e5832673b5bcbe
Thomas Barré-Pitous <barrepitousthomas@gmail.com>
2023-10-27 21:18:35 +0900

    Validation login

--- pages/login.py
+++ pages/login.py
@@ -1,8 +1,7 @@
 import dash
 from dash import html
-import time
 import dash_mantine_components as dmc
-from dash import html, Output, Input, no_update, callback
+from dash import html, Output, Input, State, no_update, callback
 from dash_iconify import DashIconify
 
 # Custom modules
@@ -19,12 +18,14 @@ login_component = (
                     label="Username",
                     placeholder="Your username",
                     icon=DashIconify(icon="radix-icons:person"),
+                    id="username-input",
                 ),
                 dmc.TextInput(
                     label="Password",
                     type="password",
                     placeholder="Your password",
                     icon=DashIconify(icon="radix-icons:lock-closed"),
+                    id="password-input",
                 ),
                 # dmc.Checkbox(
                 #     label="Remember me",
@@ -40,14 +41,24 @@ login_component = (
 
 
 layout = html.Div(
-    # style={"width": 200},
-    children=dmc.Container(
-        dmc.Center(
-            login_component,
+    children=[
+        dmc.Container(
+            [
+                dmc.Anchor(
+                    dmc.Button(
+                        "Previous",
+                        leftIcon=DashIconify(icon="uil:previous", width=20),
+                    ),
+                    href="/",
+                ),
+                dmc.Center(
+                    login_component,
+                    style={"height": "75vh"},
+                ),
+            ],
             style={"height": "100vh"},
         ),
-        style={"height": "100vh"},
-    ),
+    ],
     style={"height": "100vh"},
 )
 
@@ -60,3 +71,20 @@ layout = html.Div(
 def func(n_clicks):
     mslogin.login("", "")
     return no_update
+
+
+@callback(
+    Output("username-input", "error"),
+    Output("password-input", "error"),
+    Input("load-button", "n_clicks"),
+    State("username-input", "value"),
+    State("password-input", "value"),
+    prevent_initial_call=True,
+)
+def validate_login(n_clicks, username, password):
+    print(bool(username))
+    print(username)
+    return (
+        "Username cannot be empty." if not bool(username) else False,
+        "Password cannot be empty." if not bool(password) else False,
+    )
