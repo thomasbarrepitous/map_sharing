1be3434daf5a6774207e23e025db80daed4db3b7
Thomas Barré-Pitous <barrepitousthomas@gmail.com>
2023-10-31 23:42:58 +0900

    Added JWT

--- mapsharingapi/mapsharingapi/settings.py
+++ mapsharingapi/mapsharingapi/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from django.core.management.utils import get_random_secret_key
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +23,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*c1wova1fgo^fjl-z*7$svia*s7qr=ef=v(2(2(^-)(51y^%uk"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = bool(os.environ.get("DEBUG", False) == "True")
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
+
+# if DEBUG:
+#     print("DEBUG MODE")
+# else:
+#     print("PRODUCTION MODE")
 
 # Application definition
 
@@ -38,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -73,15 +83,21 @@ WSGI_APPLICATION = "mapsharingapi.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
+else:
+    pass
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
--- mapsharingapi/mapsharingapi/urls.py
+++ mapsharingapi/mapsharingapi/urls.py
@@ -1,6 +1,10 @@
 from django.urls import include, path
 from rest_framework import routers
 from mapsharingapi.mapsharing import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = routers.DefaultRouter()
 # router.register(r"users", views.UserViewSet)
@@ -11,4 +15,6 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path("", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
