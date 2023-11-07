2c1c70318310963f20cdd74757b59567654e6d30
Thomas Barré-Pitous <barrepitousthomas@gmail.com>
2023-11-08 02:15:46 +0900

    Change gitignore and model.py

--- mapsharingapi/mapsharingapi/mapsharing/models.py
+++ mapsharingapi/mapsharingapi/mapsharing/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
 
+class User(models.Model):
+    """
+    Model representing a user.
+    """
+
+    username = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
+
 class Playlist(models.Model):
     """
     Model representing a playlist of geocode points.
